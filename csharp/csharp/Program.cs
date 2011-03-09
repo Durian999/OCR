8b9f6556f38c3d4ef99c3a89acf793f8dc847aaa
cmath <firemat@gmail.com>
2011-03-09 12:15:26 +0100

    dodanie komenta

--- csharp/csharp/Program.cs
+++ csharp/csharp/Program.cs
@@ -13,6 +13,7 @@ namespace csharp
         [STAThread]
         static void Main()
         {
+            //cycki
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
